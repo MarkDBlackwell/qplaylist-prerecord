8edceb807fd49017424d9596ae441d055ea0ebe7
Mark D. Blackwell <markdblackwell01@gmail.com>
2018-06-28 12:46:30 -0400

    Align code

--- lib/qplaylist_prerecord/graphical.rb
+++ lib/qplaylist_prerecord/graphical.rb
@@ -15,91 +15,91 @@ require 'tk'
 module ::QplayistPrerecord
   module Graphical
 
-  extend self
+    extend self
 
-  def main
-    song_list = SongList.new
-    airshow = Airshow.new
+    def main
+      song_list = SongList.new
+      airshow = Airshow.new
 
-    @program_name = 'QPlaylistPrerecord'
-    root = TkRoot.new
-    title_set root, @program_name
-    top = TkFrame.new root
-    prompt_choice top, pack_standard
-    @button_navigate = button_navigate root, top, pack_standard
-    button_remove root, top, pack_standard
-    button_greeting root, top, pack_standard
-    button_exit top, pack_standard
-    top.pack fill: :both, side: :top
-    Tk.mainloop
-    nil
-  end
+      @program_name = 'QPlaylistPrerecord'
+      root = TkRoot.new
+      title_set root, @program_name
+      top = TkFrame.new root
+      prompt_choice top, pack_standard
+      @button_navigate = button_navigate root, top, pack_standard
+      button_remove root, top, pack_standard
+      button_greeting root, top, pack_standard
+      button_exit top, pack_standard
+      top.pack fill: :both, side: :top
+      Tk.mainloop
+      nil
+    end
 
-  private
+    private
 
-  def button_exit(top, pack_standard)
-    TkButton.new top do
-      text 'Exit'
-      command do
-        proc ::Kernel.exit
+    def button_exit(top, pack_standard)
+      TkButton.new top do
+        text 'Exit'
+        command do
+          proc ::Kernel.exit
+        end
+        pack pack_standard
       end
-      pack pack_standard
+      nil
     end
-    nil
-  end
 
-  def button_navigate(root, top, pack_standard)
-    proc_title = proc do
-      @topic_title = 'something' unless defined? @topic_title
-      s = "#{@topic_title} - #{@program_name}"
-      title_set root, s
-      @topic_title = @topic_title.chars.shuffle.join ''
+    def button_navigate(root, top, pack_standard)
+      proc_title = proc do
+        @topic_title = 'something' unless defined? @topic_title
+        s = "#{@topic_title} - #{@program_name}"
+        title_set root, s
+        @topic_title = @topic_title.chars.shuffle.join ''
+      end
+      TkButton.new top do
+        text 'Navigate'
+        command proc_title
+        pack pack_standard
+      end
     end
-    TkButton.new top do
-      text 'Navigate'
-      command proc_title
-      pack pack_standard
-    end
-  end
 
-  def button_greeting(root, top, pack_standard)
-    proc_greeting_show = proc do
+    def button_greeting(root, top, pack_standard)
+      proc_greeting_show = proc do
+      end
+      TkButton.new top do
+        text 'Show Greeting'
+        command proc_greeting_show
+        pack pack_standard
+      end
+      nil
     end
-    TkButton.new top do
-      text 'Show Greeting'
-      command proc_greeting_show
-      pack pack_standard
-    end
-    nil
-  end
 
-  def button_remove(root, top, pack_standard)
-    proc_remove = proc do
-      @button_navigate.destroy
+    def button_remove(root, top, pack_standard)
+      proc_remove = proc do
+        @button_navigate.destroy
+      end
+      TkButton.new top do
+        text 'Destroy'
+        command proc_remove
+        pack pack_standard
+      end
+      nil
     end
-    TkButton.new top do
-      text 'Destroy'
-      command proc_remove
-      pack pack_standard
+
+    def pack_standard
+      {padx: 10, pady: 10}
     end
-    nil
-  end
 
-  def pack_standard
-    {padx: 10, pady: 10}
-  end
-
-  def prompt_choice(top, pack_standard)
-    TkLabel.new top do
-      text 'Choose!'
-      pack pack_standard
+    def prompt_choice(top, pack_standard)
+      TkLabel.new top do
+        text 'Choose!'
+        pack pack_standard
+      end
+      nil
     end
-    nil
-  end
 
-  def title_set(root, title_new)
-    root.title = title_new
-    nil
-  end
+    def title_set(root, title_new)
+      root.title = title_new
+      nil
+    end
   end
 end
