ef85913ffd4329fe45f30e5cca593a17949e7c4f
Mark D. Blackwell <markdblackwell01@gmail.com>
2018-06-29 07:00:03 -0400

    Rename

--- lib/qplaylist_prerecord/graphical.rb
+++ lib/qplaylist_prerecord/graphical.rb
@@ -21,24 +21,21 @@ module ::QplayistPrerecord
       song_list = SongList.new
       airshow = Airshow.new
 
-      @program_name = 'QPlaylistPrerecord'
-      root = TkRoot.new
-      title_set root, @program_name
-      top = TkFrame.new root
+      root, top, menu, body = window_init
+      title_init root
+#     menu_init root, menu
+      menu_init root, top
+#      prompt_choice menu, pack_standard
       prompt_choice top, pack_standard
-      @button_navigate = button_navigate root, top, pack_standard
-      button_remove root, top, pack_standard
-      button_greeting root, top, pack_standard
-      button_exit top, pack_standard
-      top.pack fill: :both, side: :top
-      Tk.mainloop
+
+      window_run top
       nil
     end
 
     private
 
-    def button_exit(top, pack_standard)
-      TkButton.new top do
+    def button_exit_init(menu, pack_standard)
+      TkButton.new menu do
         text 'Exit'
         command do
           proc ::Kernel.exit
@@ -48,24 +45,10 @@ module ::QplayistPrerecord
       nil
     end
 
-    def button_navigate(root, top, pack_standard)
-      proc_title = proc do
-        @topic_title = 'something' unless defined? @topic_title
-        s = "#{@topic_title} - #{@program_name}"
-        title_set root, s
-        @topic_title = @topic_title.chars.shuffle.join ''
-      end
-      TkButton.new top do
-        text 'Navigate'
-        command proc_title
-        pack pack_standard
-      end
-    end
-
-    def button_greeting(root, top, pack_standard)
+    def button_greeting_init(menu, pack_standard)
       proc_greeting_show = proc do
       end
-      TkButton.new top do
+      TkButton.new menu do
         text 'Show Greeting'
         command proc_greeting_show
         pack pack_standard
@@ -73,16 +56,41 @@ module ::QplayistPrerecord
       nil
     end
 
-    def button_remove(root, top, pack_standard)
+    def button_navigate_init(root, menu, pack_standard)
+      proc_title = proc do
+        @topic_title = 'something' unless defined? @topic_title
+        s = "#{@topic_title} - #{@program_name}"
+        title_set root, s
+        @topic_title = @topic_title.chars.shuffle.join ''
+      end
+      TkButton.new menu do
+        text 'Navigate'
+        command proc_title
+        pack pack_standard
+      end
+    end
+
+    def button_remove_init(menu, pack_standard)
       proc_remove = proc do
         @button_navigate.destroy
       end
-      TkButton.new top do
+      TkButton.new menu do
         text 'Destroy'
         command proc_remove
         pack pack_standard
       end
       nil
+    end
+
+    def menu_init(root, menu)
+      TkMenu.new root do
+      end
+
+      @button_navigate = button_navigate_init root, menu, pack_standard
+
+      button_remove_init    menu, pack_standard
+      button_greeting_init  menu, pack_standard
+      button_exit_init      menu, pack_standard
     end
 
     def pack_standard
@@ -97,8 +105,28 @@ module ::QplayistPrerecord
       nil
     end
 
+    def title_init(root)
+      @program_name = 'QPlaylistPrerecord'
+      title_set root, @program_name
+      nil
+    end
+
     def title_set(root, title_new)
       root.title = title_new
+      nil
+    end
+
+    def window_init
+      root = TkRoot.new
+      top = TkFrame.new root
+      menu = TkFrame.new top
+      body = TkFrame.new top
+      [root, top, menu, body]
+    end
+
+    def window_run(top)
+      top.pack fill: :both, side: :top
+      Tk.mainloop
       nil
     end
   end
