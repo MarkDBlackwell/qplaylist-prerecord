41a9e76ad08d72fad9b30d32a029e95ac5160078
Mark D. Blackwell <markdblackwell01@gmail.com>
2018-07-06 16:00:54 -0400

    Read airshows list from file

--- lib/qplaylist_prerecord/airshows.rb
+++ lib/qplaylist_prerecord/airshows.rb
@@ -10,15 +10,14 @@ require 'helper'
 
 module ::QplayistPrerecord
   module Airshows
+    include Helper
     extend self
 
     def all
-      names = %w[
-          Young-At-Heart
-          Baltimore-Hit-Parade-Tue
-          Baltimore-Hit-Parade-Sun
-          ]
-      names.map{|e| Airshow.new "#{e} 0123"}.sort
+      lines = ::File.open filename_airshows do |f|
+        f.readlines.map{|e| whitespace_compress e}.reject{|e| e.empty? or e.start_with? '#'}
+      end
+      lines.map{|e| Airshow.new e}.sort
     end
 
     private
@@ -31,14 +30,6 @@ module ::QplayistPrerecord
     def filename_airshows
       basename = 'cart-numbers-airshows.txt'
       ::File.join directory_playlist, basename
-    end
-
-    def segments
-      lines = ::File.open filename_airshows do |f|
-        f.readlines.map{|e| whitespace_compress e}.reject{|e| e.empty? or e.start_with? '#'}
-      end
-      lines.map do |line|
-      end
     end
   end
 end
