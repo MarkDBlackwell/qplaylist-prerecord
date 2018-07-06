a78629a6a4405c761f19f865569eb032d21c9bf4
Mark D. Blackwell <markdblackwell01@gmail.com>
2018-07-06 15:50:25 -0400

    Toward segments

--- lib/qplaylist_prerecord/airshow.rb
+++ lib/qplaylist_prerecord/airshow.rb
@@ -13,9 +13,41 @@ module ::QplayistPrerecord
     def self.fields_ordered
       %i[
         name
+        cartridge_numbers
         ]
     end
 
     attr_reader(*fields_ordered)
+
+    def initialize(line)
+      tokens =     line.split ' '
+      @name = tokens.first
+      @cartridge_numbers = tokens.drop name_token_count
+      check tokens
+      nil
+    end
+
+    private
+
+    def cartridge_number_length
+      @@cartridge_number_length_value ||= '0123'.length
+    end
+
+    def cartridge_number_regexp
+      @@cartridge_number_regexp_value ||= ::Regexp.new '\A\d++\z', ::Regexp::MULTILINE
+    end
+
+    def check(             tokens)
+      ::Kernel.exit unless tokens.length > name_token_count
+      @cartridge_numbers.each do |cart|
+        ::Kernel.exit unless cart.length == cartridge_number_length
+        ::Kernel.exit unless cartridge_number_regexp =~ cart
+      end
+      nil
+    end
+
+    def name_token_count
+      1
+    end
   end
 end
--- lib/qplaylist_prerecord/airshows.rb
+++ lib/qplaylist_prerecord/airshows.rb
@@ -6,6 +6,7 @@ mdb July 3, 2018 - created
 =end
 
 require 'airshow'
+require 'helper'
 
 module ::QplayistPrerecord
   module Airshows
@@ -17,7 +18,27 @@ module ::QplayistPrerecord
           Baltimore-Hit-Parade-Tue
           Baltimore-Hit-Parade-Sun
           ]
-      names.map{|e| Airshow.new e}.sort
+      names.map{|e| Airshow.new "#{e} 0123"}.sort
+    end
+
+    private
+
+    def directory_playlist
+# For production, this should change:
+      directory_etc_example
+    end
+
+    def filename_airshows
+      basename = 'cart-numbers-airshows.txt'
+      ::File.join directory_playlist, basename
+    end
+
+    def segments
+      lines = ::File.open filename_airshows do |f|
+        f.readlines.map{|e| whitespace_compress e}.reject{|e| e.empty? or e.start_with? '#'}
+      end
+      lines.map do |line|
+      end
     end
   end
 end
