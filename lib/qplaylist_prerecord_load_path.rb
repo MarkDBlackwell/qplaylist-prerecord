f3f0e2d25a58d645f9b88200f0b0a8fc0c9c00cb
Mark D. Blackwell <markdblackwell01@gmail.com>
2018-10-15 13:57:36 -0400

    Organize

--- lib/qplaylist_prerecord_load_path.rb
+++ lib/qplaylist_prerecord_load_path.rb
@@ -10,17 +10,19 @@ Copyright (C) 2018 Mark D. Blackwell.
 
 require 'English'
 
-package_name = 'qplaylist_prerecord'
-section_names = %w[ ungraphical ]
+package = 'qplaylist_prerecord'
 
 dirname_file_current = ::Kernel.__dir__
 project_root = ::File.join dirname_file_current, '..'
-lib = ::File.join project_root, 'lib'
-package = ::File.join lib, package_name
-sections = section_names.map{|e| ::File.join package, e}
 
-paths = [lib, package] + sections
-paths.each do |path|
+branches = [
+    ['lib'],
+    ['lib', package],
+    ['lib', package, 'ungraphical'],
+    ['lib', package,   'graphical'],
+    ]
+branches.sort.each do |branch|
+  path = ::File.join project_root, *branch
   real = ::File.realpath path
   $LOAD_PATH.unshift real unless $LOAD_PATH.include? real
 end
