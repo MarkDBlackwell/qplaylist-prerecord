8a81edd68a6ce8f0b9cfa5385524626dcbad80a7
Mark D. Blackwell <markdblackwell01@gmail.com>
2018-10-15 12:50:35 -0400

    Startable from Windows command line

---
+++ exe/qplaylist-prerecord.bat
@@ -0,0 +1,13 @@
+@ECHO OFF
+rem Copyright (C) 2018 Mark D. Blackwell.
+rem    All rights reserved.
+rem    This program is distributed in the hope that it will be useful,
+rem    but WITHOUT ANY WARRANTY; without even the implied warranty of
+rem    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+rem For Qplaylist Prerecord
+
+rem #--------------
+rem Don't greet.
+
+@ruby "%~dpn0"
