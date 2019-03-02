ab1005268112139094ad1d03fcbb681912e48288
Mark D. Blackwell <markdblackwell01@gmail.com>
2019-03-02 16:43:06 -0500

    Delete executable files

--- exe/qplaylist-prerecord.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-rem Copyright (C) 2018 Mark D. Blackwell.
-rem    All rights reserved.
-rem    This program is distributed in the hope that it will be useful,
-rem    but WITHOUT ANY WARRANTY; without even the implied warranty of
-rem    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-
-@ruby "%~dpn0"
