21e8dff7c87671723147385758660fc4b9468823
Mark D. Blackwell <markdblackwell01@gmail.com>
2019-11-07 11:51:42 -0500

    Improve

--- lib/qplaylist_prerecord/graphical/about_information.rb
+++ lib/qplaylist_prerecord/graphical/about_information.rb
@@ -16,32 +16,33 @@ module ::QplayistPrerecord
       include HelperGraphical
       extend self
 
+      attr_accessor :show
+
       def init
-        if @show
-          @text = ::Tk::Tile::Label.new @body do
-            text <<END.chomp
-QPlaylist Prerecord #{::QplayistPrerecord::VERSION}
-Copyright (C) 2018 Mark D. Blackwell.
-No license. All rights reserved.
-Contact: markdblackwell01@gmail.com
-END
-          end
+        if show
+          @about_information_text = ::Tk::Tile::Label.new @body
+          @about_information_text.text text_about
         end
         nil
       end
 
       def pack
-        if @show
-          @text.pack pack_standard
+        if show
+          @about_information_text.pack pack_standard
         end
         nil
       end
 
-      def show=(v)
-        @show = v
-      end
-
       private
+
+      def text_about
+        @text_about_private ||= <<END.chomp
+QPlaylist Prerecord #{::QplayistPrerecord::VERSION}
+Copyright (C) 2018 Mark D. Blackwell.
+No license. All rights reserved.
+Contact: markdblackwell01@gmail.com
+END
+      end
     end
   end
 end
